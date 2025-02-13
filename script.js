1d4b820746c03cce7af8753a216929dc7aa18cdb
gbyanova <gbyanova@gmail.com>
2025-02-13 03:10:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menu-toggle");
+    const mobileMenu = document.getElementById("mobile-menu");
+    const menuLinks = document.querySelectorAll(".menu-link");
+
+    menuToggle.addEventListener("click", function () {
+        mobileMenu.classList.toggle("active");
+        menuToggle.classList.toggle("open");
+    });
+
+    menuLinks.forEach(link => {
+        link.addEventListener("click", function () {
+            mobileMenu.classList.remove("active");
+        })
+    })
+});
